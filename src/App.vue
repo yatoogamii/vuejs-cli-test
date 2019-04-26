7cfe394d3ca31a9d18100f30ea659050486337be
yatoogamii <bracaval.elias.pro@gmail.com>
2019-04-26 12:12:16 +0200

    remove rest of first test character.vue and add img for user and monster in assets/img

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
   },
   data() {
     return {
-      newGame: false
+      newGame: false,
     };
   },
   methods: {
@@ -28,11 +28,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: space-evenly;
 }
 </style>
